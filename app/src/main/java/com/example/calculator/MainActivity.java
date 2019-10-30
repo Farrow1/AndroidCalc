456e3afc12814a648081f7cc48eaa32fd48093f0
Jack Allen <Allenj25@nku.edu>
2019-10-29 22:34:22 -0400

    initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0, btn_Add, btn_Sub, btn_Mul, btn_Div, btn_equal, btn_dec, btn_clear;
+    EditText edit1;
+    float Value1, Value2;
+    boolean add, sub, mul, div;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_0 = (Button) findViewById(R.id.btn_0);
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_5 = (Button) findViewById(R.id.btn_5);
+        btn_6 = (Button) findViewById(R.id.btn_6);
+        btn_7 = (Button) findViewById(R.id.btn_7);
+        btn_8 = (Button) findViewById(R.id.btn_8);
+        btn_9 = (Button) findViewById(R.id.btn_9);
+        btn_Add = (Button) findViewById(R.id.btn_Add);
+        btn_Sub= (Button) findViewById(R.id.btn_Sub);
+        btn_Mul = (Button) findViewById(R.id.btn_mul);
+        btn_Div = (Button) findViewById(R.id.btn_div);
+        btn_equal = (Button) findViewById(R.id.btn_equals);
+        btn_dec = (Button) findViewById(R.id.btn_dec);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        edit1 = (EditText) findViewById(R.id.edit1);
+
+        btn_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText((edit1.getText())+"0");
+            }
+        });
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "1");
+            }
+        });
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "2");
+            }
+        });
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "3");
+            }
+        });
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "4");
+            }
+        });
+        btn_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "5");
+            }
+        });
+        btn_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "6");
+            }
+        });
+        btn_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "7");
+            }
+        });
+        btn_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "8");
+            }
+        });
+        btn_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + "9");
+            }
+        });
+        btn_dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit1.setText(edit1.getText() + ".");
+            }
+        });
+        btn_Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Value1 = edit1.getText();
+            }
+        });
+    }
+}
